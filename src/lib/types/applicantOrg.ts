6f5a1fa7d59ec17b81ce0cb50dd9e1f2ef08d81a
Ben Denzer <denzer.ben@gmail.com>
2026-02-16 00:03:29 -0800

    add applicantOrg type/helpers

---
+++ src/lib/types/applicantOrg.ts
@@ -0,0 +1,39 @@
+import z from 'zod';
+
+export const DBApplicantOrgInputSchema = z.object({
+  company_name: z.string().min(1).max(255),
+  primary_contact_name: z.string().min(1).max(255),
+  primary_contact_email: z.email(),
+});
+
+export type DBApplicantOrgInput = z.infer<typeof DBApplicantOrgInputSchema>;
+
+export const DBApplicantOrgSchema = DBApplicantOrgInputSchema.extend({
+  id: z.number(),
+  created_at: z.string(),
+  updated_at: z.string(),
+});
+
+export type DBApplicantOrg = z.infer<typeof DBApplicantOrgSchema>;
+
+export const ApplicantOrgSchema = z.object({
+  id: z.number(),
+  companyName: z.string().min(1).max(255),
+  primaryContactName: z.string().min(1).max(255),
+  primaryContactEmail: z.email(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
+export type ApplicantOrg = z.infer<typeof ApplicantOrgSchema>;
+
+export const mapDBApplicantOrgToApplicantOrg = (dbApplicantOrg: DBApplicantOrg): ApplicantOrg => {
+  return {
+    id: dbApplicantOrg.id,
+    companyName: dbApplicantOrg.company_name,
+    primaryContactName: dbApplicantOrg.primary_contact_name,
+    primaryContactEmail: dbApplicantOrg.primary_contact_email,
+    createdAt: dbApplicantOrg.created_at,
+    updatedAt: dbApplicantOrg.updated_at,
+  };
+};
